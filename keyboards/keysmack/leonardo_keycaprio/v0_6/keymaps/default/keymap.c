ac3672fc54f6d68a69f9b01129fde94c130f0ba8
Stephan (Rollercole) Møller <stephanryer@hotmail.com>
2026-09-03 10:27:39 +0200

    added a boot key

--- keyboards/keysmack/leonardo_keycaprio/v0_6/keymaps/default/keymap.c
+++ keyboards/keysmack/leonardo_keycaprio/v0_6/keymaps/default/keymap.c
@@ -10,7 +10,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * ├───┼───┼───┼───┼───┤           ├───┼───┼───┼───┼───┤
      * │ F │ A │ S │ T │ G │           │ M │ N │ E │ I │ Y │
      * ├───┼───┼───┼───┼───┤           ├───┼───┼───┼───┼───┤
-     * │ Z │ X │ C │ D │ V │           │ J │ H │ , │ . │ Z │
+     * │ Z │ X │ C │ D │ V │           │ J │ H │ , │ . │BOOT│
      * └───┴───┴───┴───┴───┘           └───┴───┴───┴───┴───┘
      *           ┌─────┬─────┐       ┌─────┬─────┐
      *           │  O  │ ENT │       │ SPC │  I  │
@@ -19,7 +19,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT(
         KC_Q,    KC_W,    KC_R,    KC_P,    KC_B,             KC_K,    KC_L,    KC_O,    KC_U,    KC_QUOTE,
         KC_F,    KC_A,    KC_S,    KC_T,    KC_G,             KC_M,    KC_N,    KC_E,    KC_I,    KC_Y,
-        KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,             KC_J,    KC_H,    KC_COMMA,KC_DOT,  KC_Z,
+        KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,             KC_J,    KC_H,    KC_COMMA,KC_DOT,  QK_BOOT,
                           KC_O,    KC_ENTER,                   KC_SPACE,KC_I
     )
 };
