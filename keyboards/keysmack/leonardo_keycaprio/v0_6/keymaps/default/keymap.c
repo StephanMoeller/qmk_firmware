dd2b3b694ebba28377014ed623ed47c2d3d157e2
Stephan (Rollercole) Møller <stephanryer@hotmail.com>
2026-09-03 10:23:58 +0200

    added v0

--- keyboards/keysmack/leonardo_keycaprio/v0_6/keymaps/default/keymap.c
+++ keyboards/keysmack/leonardo_keycaprio/v0_6/keymaps/default/keymap.c
@@ -5,17 +5,21 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /*
-     * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┐
-     * │ Q │ W │ E │ R │ T │ Y │ U │ I │ O │ P │
-     * ├───┼───┼───┼───┼───┼───┼───┼───┼───┼───┤
-     * │ A │ S │ D │ F │ G │ H │ J │ K │ L │ ; │
-     * ├───┼───┼───┼───┼───┼───┼───┼───┼───┼───┤
-     * │ Z │ X │ C │ V │ B │ N │ M │ , │ . │ / │
-     * └───┴───┴───┴───┴───┴───┴───┴───┴───┴───┘
+     * ┌───┬───┬───┬───┬───┐           ┌───┬───┬───┬───┬───┐
+     * │ Q │ W │ R │ P │ B │           │ K │ L │ O │ U │ ' │
+     * ├───┼───┼───┼───┼───┤           ├───┼───┼───┼───┼───┤
+     * │ F │ A │ S │ T │ G │           │ M │ N │ E │ I │ Y │
+     * ├───┼───┼───┼───┼───┤           ├───┼───┼───┼───┼───┤
+     * │ Z │ X │ C │ D │ V │           │ J │ H │ , │ . │ Z │
+     * └───┴───┴───┴───┴───┘           └───┴───┴───┴───┴───┘
+     *           ┌─────┬─────┐       ┌─────┬─────┐
+     *           │  O  │ ENT │       │ SPC │  I  │
+     *           └─────┴─────┘       └─────┴─────┘
      */
-    [0] = LAYOUT_ortho_3x10(
-        KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,
-        KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN,
-        KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH
+    [0] = LAYOUT(
+        KC_Q,    KC_W,    KC_R,    KC_P,    KC_B,             KC_K,    KC_L,    KC_O,    KC_U,    KC_QUOTE,
+        KC_F,    KC_A,    KC_S,    KC_T,    KC_G,             KC_M,    KC_N,    KC_E,    KC_I,    KC_Y,
+        KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,             KC_J,    KC_H,    KC_COMMA,KC_DOT,  KC_Z,
+                          KC_O,    KC_ENTER,                   KC_SPACE,KC_I
     )
 };
